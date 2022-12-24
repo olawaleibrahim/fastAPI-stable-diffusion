d77f8035634f53d11544f3232e7b608b14b6ba1e
Olawale Ibrahim <ibrahim.olawale13@gmail.com>
2022-12-24 13:47:53 +0100

    fastapi app and obtain image from model function

--- ml.py
+++ ml.py
@@ -16,3 +16,22 @@ pipe = StableDiffusionPipeline.from_pretrained(
 
 pipe.to("cuda")
 prompt = "a photograph of an astronaut riding a whale"
+image = pipe(prompt)["sample"][0]
+
+
+def obtain_image(
+    prompt: str,
+    *,
+    seed=None,
+    num_inference_steps: int = 50,
+    guidance_scale: float = 7.5
+) -> Image:
+    generator = None if seed is None else torch.Generator("cuda")
+    print(f"Using device: {pipe.device}")
+    image: Image = pipe(
+        prompt,
+        guidance_scale=guidance_scale,
+        num_inference_steps=num_inference_steps,
+        generator=generator,
+    ).images[0]
+    return image
