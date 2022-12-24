e42273035392c4e462bf2b16a480df338ca705f9
Olawale Ibrahim <ibrahim.olawale13@gmail.com>
2022-12-24 13:17:49 +0100

    instantiate diffusion model

---
+++ ml.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import torch
+from diffusers import StableDiffusionPipeline
+from PIL.Image import Image
+
+token_path = Path("token.txt")
+token = token_path.read_text().strip()
+
+pipe = StableDiffusionPipeline.from_pretrained(
+    "CompVis/stable-diffusion-v1-4",
+    revision="fp16",
+    torch_dtype=torch.float16,
+    use_auth_token=token
+)
+
+pipe.to("cuda")
+prompt = "a photograph of an astronaut riding a whale"
